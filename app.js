49609db0d494c9eb3ed03edde9a424f040bc2db0
unknown <syyadkhan@yahoo.com>
2018-04-29 19:00:08 -0400

    apr291900

--- app.js
+++ app.js
@@ -64,6 +64,22 @@ return {
        return newItem;
     },
 
+
+    deleteItem: function(type, id){
+        var ids, index;
+
+        ids = data.allItems[type].map(function(current){
+            return current.id;
+        });
+
+        index = ids.indexOf(id);
+
+    if (index !== -1){
+        data.allItems[type].splice(index, 1);
+        }        
+    },
+
+
     calculateBudget: function(){
 
 
@@ -150,6 +166,17 @@ var UIController = (function(){
 
 
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+
+
+
+
+
+        deleteListItem: function(selectorID){
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+            
         },
 
         clearFields: function(){
@@ -253,7 +280,18 @@ var ctrlDeleteItem = function(event){
 
         splitID = itemID.split('-');
         type = splitID[0];
-        ID = splitID[1];
+        ID = parseInt(splitID[1]);
+
+
+
+
+
+
+
+
+
+        budgetCtrl.deleteItem(type, ID);
+
     }
 
 };
